6468beef7b54cb1d443b711c77d4d138d27c057b
Meredith <124798853+Mer-W@users.noreply.github.com>
2023-11-22 11:26:20 -0500

    progress on login + registration
    - partial implementation
    todo:
    - complete validation and error handling
    - option to sign in with username OR email
    - redirect after success
    - signup page layout/css

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,12 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TwitterClone.Models;
 
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 namespace TwitterClone.Pages
+
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        private readonly SignInManager<User> signInManager;
+        private readonly ILogger<LoginModel> logger;
+
+        public LoginModel(SignInManager<User> signInManager, ILogger<LoginModel> logger)
         {
+            this.signInManager = signInManager;
+            this.logger = logger;
         }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            public string UserName { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+
+                var result = await signInManager.PasswordSignInAsync(Input.UserName, Input.Password, false, true);
+                if (result.Succeeded)
+                {
+                    logger.LogInformation($"User {Input.UserName} logged in");
+                    return RedirectToPage("/Index"); //TODO redirect user to home or attemped page
+                }
+                else
+                { // user does not exist, password invalid, account locked out
+                    ModelState.AddModelError(string.Empty, "Login failed (user does not exist, password invalid, or account locked out)");
+                }
+            }
+            return Page();
+        }
+
     }
 }
--- Pages/Signup.cshtml.cs
+++ Pages/Signup.cshtml.cs
@@ -1,12 +1,94 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using TwitterClone.Models;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace TwitterClone.Pages
 {
     public class SignupModel : PageModel
     {
+
+        private readonly UserManager<User> userManager;
+        private readonly ILogger<SignupModel> logger;
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public SignupModel(UserManager<User> userManager,
+            ILogger<SignupModel> logger)
+        {
+            this.userManager = userManager;
+            this.logger = logger;
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+
+            [Required]
+            [Display(Name = "Name")]
+
+            public string NickName { get; set; }
+
+            [Required]
+            [Display(Name = "Date of birth")]
+            [DataType(DataType.Date)]
+            //TODO validate date range
+            public DateTime DOB { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email address")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+
         public void OnGet()
         {
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = Input.UserName, Email = Input.Email, NickName = Input.NickName, DateOfBirth = Input.DOB, EmailConfirmed = false };
+                var result = await userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    var result2 = await userManager.AddToRoleAsync(user, "User");
+                    if (result2.Succeeded)
+                    {
+                        logger.LogInformation($"User {Input.Email} create a new account with password");
+                        return RedirectToPage("Index"); //FIXME
+                    }
+                    else
+                    {
+                        // FIXME: delete the user since role assignment failed, log the event, show error to the user
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return Page();
+        }
+
     }
 }
