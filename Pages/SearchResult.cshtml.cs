5513bf26c20f6e5ab95f4370433ee6720f80e529
xiaoxing pan <124628809+xiaoxingpan@users.noreply.github.com>
2023-11-26 23:43:46 -0500

    search people

--- Pages/SearchResult.cshtml.cs
+++ Pages/SearchResult.cshtml.cs
@@ -17,13 +17,50 @@ namespace TwitterClone.Pages
         private readonly TwitterCloneDbContext context;
         private readonly UserManager<User> userManager;
 
+        [BindProperty]
+        public List<User> SearchedUser { get; set; } = null!;
 
+        public SearchResultModel(ILogger<SearchResultModel> logger, TwitterCloneDbContext context, UserManager<User> userManager)
+        {
+            this.logger = logger;
+            this.context = context;
+            this.userManager = userManager;
+        }
 
-
-        public void OnGet(string term)
+        public async Task OnGetAsync(string term)
         {
             SearchTerm = term;
+            // get users where username, nickname, or description contains search term, case ignored
+            Console.WriteLine("Search Term: " + SearchTerm);
+
+            if (SearchTerm != null)
+            {
+                var searchedUser = context.Users
+        .AsEnumerable() // Switch to client-side evaluation
+        .Where(u =>
+            (u.NickName != null && u.NickName.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase)) ||
+            (u.Description != null && u.Description.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase)) ||
+            (u.UserName != null && u.UserName.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase)))
+        .ToList();
+                Console.WriteLine("---------Searched User: " + searchedUser.Count);
+
+                if (searchedUser.Count > 0)
+                {
+                    SearchedUser = searchedUser;
+                    Console.WriteLine("Searched User: " + SearchedUser.Count);
+                }
+                {
+                    // No users found, handle this case
+                    SearchedUser = new List<User>();
+                    Console.WriteLine("No users found");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No users found");
+            }
         }
+
     }
 }
 
